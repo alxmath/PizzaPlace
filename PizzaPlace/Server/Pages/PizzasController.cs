f41a2c75b746a50633c39dc581f2467293956f3a
Alex Santos <alxmath@gmail.com>
2022-01-19 13:43:17 -0300

    Creating Your First Code-First Migration

---
+++ PizzaPlace/Server/Pages/PizzasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPlace.Shared;
+
+namespace PizzaPlace.Server.Pages
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PizzasController : Controller
+    {
+        private static readonly List<Pizza> _pizza = new()
+        {
+            new Pizza(1, "Pepperoni", 8.99M, Spiciness.Spicy),
+            new Pizza(2, "Margarita", 7.99M, Spiciness.None),
+            new Pizza(3, "Diabolo", 9.99M, Spiciness.Hot),
+        };
+
+        [HttpGet("/pizzas")]
+        public IQueryable<Pizza> GetPizzas() =>
+            _pizza.AsQueryable();
+    }
+}
---
+++ PizzaPlace/Server/PizzaPlaceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaPlace.Shared;
+
+namespace PizzaPlace.Server
+{
+    public class PizzaPlaceDbContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; } = default!;
+
+        public PizzaPlaceDbContext(DbContextOptions<PizzaPlaceDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var pizzaEntity = modelBuilder.Entity<Pizza>();
+
+            pizzaEntity.HasKey(pizza => pizza.Id);
+            pizzaEntity.Property(pizza => pizza.Name).HasMaxLength(80);
+            pizzaEntity.Property(pizza => pizza.Price).HasColumnType("money");
+            pizzaEntity.Property(pizza => pizza.Spiciness).HasConversion<string>();
+        }
+    }
+}
